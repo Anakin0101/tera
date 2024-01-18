a17677b3765d8cdc2ea1944df4b5f89332a80543
Medea Samniashvili <medea.samniashvili@terabank.ge>
2024-01-18 11:22:31 +0400

    Merged PR 1372: fixes easy login modal extra toggle
    fixes easy login modal extra toggle
    
    https://trello.com/c/u6lIbDxM/119-open-settings-modal-%E1%83%96%E1%83%94-%E1%83%90%E1%83%A0-%E1%83%A3%E1%83%9C%E1%83%93%E1%83%90-%E1%83%A9%E1%83%90%E1%83%9C%E1%83%93%E1%83%94%E1%83%A1-%E1%83%90%E1%83%A6%E1%83%90%E1%83%A0-%E1%83%A8%E1%83%94%E1%83%9B%E1%83%9D%E1%83%9B%E1%83%97%E1%83%90%E1%83%95%E1%83%90%E1%83%96%E1%83%9D-%E1%83%97%E1%83%9D%E1%83%92%E1%83%9A%E1%83%98

--- src/components/modals/EasyLoginModal/types/EasyLoginModal.types.ts
+++ src/components/modals/EasyLoginModal/types/EasyLoginModal.types.ts
@@ -1,7 +1,16 @@
 export type EasyLoginModalProps = {
+  type: EasyLoginModalType;
   handlePress?: () => void;
+  cancelOnly?: boolean;
   title?: string;
   description?: string;
   primaryButtonText?: string;
   secondaryButtonText?: string;
 };
+
+export const easyLoginModalType = {
+  ENABLE: 'enable',
+  ACTIVATE: 'activate',
+};
+
+export type EasyLoginModalType = 'enable' | 'activate';
