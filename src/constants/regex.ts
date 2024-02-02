0947b3d60fb0875a59457d796578bf6e183ece38
Levan Gurbeleishvili <levan.gurbeleishvili@terabank.ge>
2024-02-02 10:33:46 +0400

    chore: remove regex file

--- src/constants/regex.ts
+++
@@ -1,7 +0,0 @@
-export const REGEX = {
-  MAX_LENGTH_11: /^\d{11}$/,
-  MAX_LENGTH_9: /^\d{9}$/,
-  EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-  COMMA_OR_PERIOD: /[.,]/g,
-  MATCH_ID: /id:(\d+)/,
-};
