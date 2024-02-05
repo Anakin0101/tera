15690b492bd0c8bcc9e8290f1e86ef92d56da75d
Levan Gurbeleishvili <levan.gurbeleishvili@terabank.ge>
2024-02-05 19:07:25 +0400

    chore: update ui parts

--- src/components/Payments/PaymentFieldInput/PaymentFieldInput.styles.ts
+++ src/components/Payments/PaymentFieldInput/PaymentFieldInput.styles.ts
@@ -1,26 +1,31 @@
 import { StyleSheet } from 'react-native';
-// import { useTheme } from 'hooks';
-// import { FontFamily, FontSize } from 'theme/Variables';
+import { useTheme } from 'hooks';
+import { FontFamily, FontSize } from 'theme/Variables';
 
 export const useStyles = () => {
-  // const { Colors, Layout, Spacing } = useTheme();
+  const { Colors, Layout, Spacing } = useTheme();
 
   return StyleSheet.create({
-    // title: {
-    //   fontSize: FontSize.tiny,
-    //   lineHeight: 16,
-    //   color: Colors.textBlack,
-    //   fontFamily: FontFamily.medium,
-    //   letterSpacing: -0.5,
-    //   fontWeight: '400',
-    // },
-    // desc: {
-    //   fontSize: FontSize.small,
-    //   lineHeight: 18,
-    //   color: Colors.textGray700,
-    //   fontFamily: FontFamily.medium,
-    //   letterSpacing: -0.2,
-    //   fontWeight: '400',
-    // },
+    container: {
+      ...Layout.row,
+      ...Layout.alignItemsCenter,
+      marginTop: Spacing.xlg,
+      ...Layout.justifyContentBetween,
+    },
+    headerTitle: {
+      fontSize: FontSize.small,
+      lineHeight: 16,
+      color: Colors.textBlack500,
+      fontFamily: FontFamily.medium,
+      letterSpacing: -0.2,
+      flex: 1,
+    },
+    valueStyle: {
+      fontSize: FontSize.regular,
+      lineHeight: 24,
+      color: Colors.textBlack,
+      fontFamily: FontFamily.medium,
+      letterSpacing: -0.2,
+    },
   });
 };
