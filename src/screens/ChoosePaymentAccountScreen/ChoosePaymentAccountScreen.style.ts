3d86623d98b63ba55d4cfb8e876c1b6b3a2d0fa2
Levan Gurbeleishvili <levan.gurbeleishvili@terabank.ge>
2024-02-14 04:07:32 +0400

    feat: add new screen

---
+++ src/screens/ChoosePaymentAccountScreen/ChoosePaymentAccountScreen.style.ts
@@ -0,0 +1,88 @@
+import useTheme from 'hooks/useTheme';
+import { StyleSheet } from 'react-native';
+import { FontFamily, FontSize } from 'theme/Variables';
+
+export const useStyles = () => {
+  const { Colors, Spacing, Layout } = useTheme();
+
+  return StyleSheet.create({
+    container: {
+      ...Layout.fill,
+    },
+    containerStyle: {
+      flex: 1,
+      backgroundColor: Colors.white,
+    },
+    wrapper: {
+      backgroundColor: Colors.dashboardBackground,
+    },
+    headerTitle: {
+      marginBottom: Spacing.xl,
+      marginTop: Spacing.xlm,
+      fontSize: FontSize.regularPlus,
+      letterSpacing: -0.5,
+      color: Colors.textBlack,
+      fontFamily: FontFamily.medium,
+      lineHeight: 22,
+    },
+    ctaWrapper: {
+      marginBottom: Spacing.ml,
+      marginHorizontal: Spacing.xl,
+    },
+    ctaOpenWrapper: {
+      paddingVertical: Spacing.md,
+    },
+    headerWrapper: {
+      ...Layout.fill,
+      flexWrap: 'nowrap',
+      paddingVertical: Spacing.xxs,
+      paddingRight: Spacing.xl,
+      marginLeft: Spacing.xxs,
+    },
+    providerHeaderTitle: {
+      fontSize: FontSize.small,
+      letterSpacing: -0.5,
+      color: Colors.textBlack500,
+      fontFamily: FontFamily.medium,
+      lineHeight: 16,
+    },
+    providerWrapper: {
+      backgroundColor: Colors.white,
+      borderRadius: 12,
+      marginTop: Spacing.xl,
+      ...Layout.row,
+      ...Layout.alignItemsCenter,
+      paddingHorizontal: Spacing.xl,
+      paddingVertical: Spacing.s,
+      minHeight: 60,
+    },
+    headerDesc: {
+      fontSize: FontSize.regular,
+      letterSpacing: -0.5,
+      color: Colors.textBlack,
+      fontFamily: FontFamily.medium,
+      lineHeight: 24,
+      marginTop: Spacing.xxs,
+    },
+    iconStyle: {
+      width: 48,
+      height: 48,
+      borderWidth: 1,
+      borderColor: Colors.borderColor,
+    },
+    buttonWrapper: {
+      marginTop: Spacing.xl,
+    },
+    contentWrapper: {
+      backgroundColor: Colors.white,
+      marginTop: Spacing.xxl,
+      borderTopRightRadius: 24,
+      borderTopLeftRadius: 24,
+      paddingHorizontal: Spacing.xl,
+      ...Layout.fill,
+    },
+    disabledButton: {
+      opacity: 0.5,
+    },
+  });
+};
---
+++ src/screens/ChoosePaymentAccountScreen/container.ts
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import {
+  useDebtVerifyResultsMutation,
+  useGetDebtVerifyBasketMutation,
+} from 'services/apis/paymentsAPI/paymentsAPI';
+import { getValue } from 'storage/index';
+import { SELECTED_LANGUAGE } from 'storage/constants';
+import {
+  LanguageKeyForAPIEnum,
+  LanguageKeys,
+} from 'components/LanguageSwitcher/LanguageSwitcher.types';
+import { PaymentFieldValue, Provider } from 'services/apis/paymentsAPI/paymentsAPI.types';
+import { useNavigation } from '@react-navigation/native';
+import { ModalStackScreenProps } from 'navigation/types';
+import { NEW_AUTOMATIC_PAYMENT_SCREEN } from 'navigation/ScreenNames';
+import { SubscriberFieldsValue } from './ChoosePaymentAccountScreen.types';
+
+export const useCheckProviderInfo = (providerItem: Provider) => {
+  const savedLanguage = getValue(SELECTED_LANGUAGE);
+  const { navigate } = useNavigation<ModalStackScreenProps<'NewAutomaticPaymentScreen'>>();
+  const [subscriberFieldsValue, setSubscriberFieldsValue] = useState<SubscriberFieldsValue>([]);
+
+  const [getDebtVerifyBasket, { data: debtVerifyBasketInfo, isLoading }] =
+    useGetDebtVerifyBasketMutation();
+
+  const [getDebtVerifyResults, { data: debtVerifyInfo, isLoading: isDebtVerifyLoading }] =
+    useDebtVerifyResultsMutation();
+
+  useEffect(() => {
+    getDebtVerifyBasket({
+      serviceId: providerItem?.id,
+      culture:
+        savedLanguage === LanguageKeys.geo ? LanguageKeyForAPIEnum.KA : LanguageKeyForAPIEnum.EN,
+    });
+  }, [getDebtVerifyBasket, providerItem?.id, savedLanguage]);
+
+  const getDebtVerifyResultsHandler = (
+    fieldValues: Array<PaymentFieldValue>,
+    isAutomaticPayment?: boolean,
+  ) => {
+    getDebtVerifyResults({
+      fieldValues,
+      serviceId: providerItem?.id,
+      culture:
+        savedLanguage === LanguageKeys.geo ? LanguageKeyForAPIEnum.KA : LanguageKeyForAPIEnum.EN,
+    })
+      .unwrap()
+      .then(res => {
+        if (isAutomaticPayment) {
+          navigate(NEW_AUTOMATIC_PAYMENT_SCREEN, {
+            providerItem,
+            subscriberFieldsValue,
+            debtVerifyResults: res?.debtVerifyResults || [],
+          });
+        }
+      });
+  };
+
+  return {
+    debtVerifyBasketInfo,
+    isLoading,
+    isDebtVerifyLoading,
+    getDebtVerifyResultsHandler,
+    debtVerifyResults: debtVerifyInfo?.debtVerifyResults || [],
+    subscriberFieldsValue,
+    setSubscriberFieldsValue,
+  };
+};
