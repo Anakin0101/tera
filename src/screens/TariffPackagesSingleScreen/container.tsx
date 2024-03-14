5f178de531e833f27f7f8801c2fd53573293bf9d
eto ivanelashvili <eteri.ivanelashvili@terabank.ge>
2024-03-14 17:40:23 +0400

    toast issue

--- src/screens/TariffPackagesSingleScreen/container.tsx
+++ src/screens/TariffPackagesSingleScreen/container.tsx
@@ -33,7 +33,7 @@ export const useTariffPackagesSingle = () => {
       .unwrap()
       .catch(error => {
         console.warn('Error sending OTP:', error);
-        openToast(t('newDeposit.error'), 'error');
+        openToast(error?.data?.title?.length ? error?.data?.title : 'newDeposit.error', 'error');
       });
 
     openModal({
@@ -60,7 +60,10 @@ export const useTariffPackagesSingle = () => {
                 })
                 .catch(error => {
                   console.warn('Error activating package:', ErrorUtils);
-                  openToast(error.data.title, 'error');
+                  openToast(
+                    error?.data?.title?.length ? error?.data?.title : t('newDeposit.error'),
+                    'error',
+                  );
                   closeModal();
                 });
             }
