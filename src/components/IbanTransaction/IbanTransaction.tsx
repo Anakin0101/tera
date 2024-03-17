111b48c9e0e97753c371997b4ef46656ecbce742
Anzor Mumladze <anzor.mumladze@tera.ge>
2024-03-17 15:09:03 +0400

    fix(transfers): keyboard auto-opening on transfer amount screen

--- src/components/IbanTransaction/IbanTransaction.tsx
+++ src/components/IbanTransaction/IbanTransaction.tsx
@@ -39,10 +39,8 @@ import { useKeyboard } from 'utils/useKeyboard';
 // import { REGEX } from 'constants/index';
 // import { RecepientNumberType } from 'components/PersonalNumberTransaction/PersonalNumberTransaction.types';
 import { CurrencyEnum } from 'services/apis/transfersAPI/transfersAPI.types';
-import { useIsFocused } from '@react-navigation/native';
 const IbanTransaction = () => {
   const dispatch = useAppDispatch();
-  const isFocused = useIsFocused();
   const { t } = useTranslation();
   const styles = useStyles();
   const selectedItemFromStore = useAppSelector(
@@ -183,13 +181,17 @@ const IbanTransaction = () => {
     setApiCallInitiated(false);
   }, [setApiCallInitiated]);
 
-  useEffect(() => {
-    if (typedAccountName) {
-      setTimeout(() => {
-        inputRef?.current?.focus();
-      }, 300);
-    }
-  }, [typedAccountName, isFocused]);
+  useFocusEffect(
+    useCallback(() => {
+      const focus = () => {
+        setTimeout(() => {
+          inputRef?.current?.focus();
+        }, 300);
+      };
+      focus();
+      return focus;
+    }, []),
+  );
 
   const navigateToTransferScreen = () => {
     if (data?.bicCode !== TERRA_BANK_CODE && !receiver) {
